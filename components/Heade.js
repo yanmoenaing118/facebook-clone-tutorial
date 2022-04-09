0fd661db5e131a9ea6b81e8edbd732c124b4a6f9
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-04-09 11:18:00 +0630

    header center

--- components/Heade.js
+++ components/Heade.js
@@ -15,9 +15,11 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
+import HeaderIcon from "../components/HeaderIcon";
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex bg-white items-center sticky top-0 z-50 p-2 lg:px-5 shadow-md">
       {/** left */}
       <div className="flex items-center">
         <Image
@@ -29,13 +31,29 @@ export default function Header() {
 
         <div className="flex items-center ml-2 rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
-          <input type="text" placeholder="Search Facebook" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"/>
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+          />
         </div>
       </div>
 
       {/** center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex items-center space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/** right */}
+      <div className="flex items-center sm:space-x-2">
+        <p className="font-semibold pr-3 whitespace-nowrap">Yan Moe Naing</p>
+      </div>
     </header>
   );
 }
